e0fbda655b44341e76d270ec6e27f91e9c86110f
hisg123 <hisg1201@naver.com>
2023-07-30 19:43:12 +0900

    SOLVING : 10025.js

--- 누적합/10025.js
+++ 누적합/10025.js
@@ -7,31 +7,25 @@ const input = `4 3
 
 const [N, K] = input.shift(0).split(' ').map(n => parseInt(n));
 const dict = {}
+let max_idx = 1;
 input.forEach(n => {
+    max_idx = Math.max(max_idx, parseInt(n.split(' ')[1]));
     dict[parseInt(n.split(' ')[1])] = parseInt(n.split(' ')[0]);
 });
 
 const solution = () => {
-    console.log(N, K);
-    console.log(dict);
     let sum = 0;
-    let max_idx = 1;
     for (let i = 1; i <= K; i++) {
         if (dict[i]) {
-            max_idx = Math.max(max_idx, i);
             sum += dict[i];
         }
-        console.log("🚀 ~ solution ~ max_idx:", max_idx)
     }
 
-    
-
-    let max = sum;
+    let max = sum;    
     for (let i = 1; i <= max_idx-K; i++) {
-        sum -= dict[i] ?? 0;
-        sum += dict[i+K] ?? 0;
+        sum -= dict[i-(K+1)] ?? 0;
+        sum += dict[i+(K+1)] ?? 0;
         max = Math.max(max, sum);
-        console.log(sum);
     }
     return max;
 }
