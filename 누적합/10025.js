7651b2be0267ec09e762cfdbc359422c5c542318
hisg123 <hisg1201@naver.com>
2023-07-31 21:10:35 +0900

    SOLVE : 10025_2nd.js

--- 누적합/10025.js
+++ 누적합/10025.js
@@ -8,6 +8,7 @@ const input = `4 3
 const [N, K] = input.shift(0).split(' ').map(n => parseInt(n));
 const dict = {}
 let max_idx = 1;
+//좌표에 따른 얼음 무게 JSON 형식으로 만듬
 input.forEach(n => {
     max_idx = Math.max(max_idx, parseInt(n.split(' ')[1]));
     dict[parseInt(n.split(' ')[1])] = parseInt(n.split(' ')[0]);
@@ -15,16 +16,18 @@ input.forEach(n => {
 
 const solution = () => {
     let sum = 0;
-    for (let i = 1; i <= K; i++) {
+    //0~K까지 얼음무게 합 구함
+    for (let i = 0; i < K+1; i++) {
         if (dict[i]) {
             sum += dict[i];
         }
     }
 
+    //0~길이 까지 최대 얼음무게 구함
     let max = sum;    
-    for (let i = 1; i <= max_idx-K; i++) {
-        sum -= dict[i-(K+1)] ?? 0;
-        sum += dict[i+(K+1)] ?? 0;
+    for (let i = 0; i < max_idx; i++) {
+        sum -= dict[i-(K+1)] ? dict[i-(K+1)] : 0;
+        sum += dict[i+(K+1)] ? dict[i+(K+1)] : 0;
         max = Math.max(max, sum);
     }
     return max;
---
+++ 누적합/10025_2nd.js
@@ -0,0 +1,42 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = `4 3
+4 7
+10 15
+2 2
+5 1`.split('\n');
+
+const [N, K] = input.shift().split(' ');
+const arrDict = {};
+const arr = input.map(v => v.split(' '));
+let max_pos = 0;
+
+//position별로 ice value를 dict로
+arr.forEach(v => {
+    arrDict[v[1]] = parseInt(v[0]);
+    max_pos = Math.max(max_pos, v[1]);
+})
+
+const solution = (arrDict, n, k) => {
+    let sum = 0;
+    //처음에는 0~k만큼 초기값을 구해둔다.
+    for (let i = 0; i < k + 1; i++) {
+        sum += arrDict[i] ? arrDict[i] : 0;
+    }
+
+    //가장 큰 position까지 
+    // i-(k+1)의 얼음 값 빼고,
+    // i+(k+1)의 얼음 값 더한다.
+    // 그렇게 max 값 계속 비교
+    let max = sum;
+    for (let i = 0; i < max_pos + 1; i++) {
+        sum -= arrDict[i - k - 1] ? arrDict[i - k - 1] : 0;
+        sum += arrDict[i + k + 1] ? arrDict[i + k + 1] : 0;
+        max = Math.max(max, sum);
+        console.log(i - k - 1, i, i + k + 1, max);
+    }
+    return max;
+}
+
+
+console.log(solution(arrDict, N, parseInt(K)))
+
