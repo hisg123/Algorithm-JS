fccfc440ba63e79e04cb5b87c5a49b94be3cb8e2
hisg123 <hisg1201@naver.com>
2023-08-13 17:00:48 +0900

    SOLVE 6558

--- 수학/6558골드바흐의추측2.js
+++ 수학/6558골드바흐의추측2.js
@@ -1,37 +1,41 @@
-const input = `8
-20
-42
-6
-0`.split('\n').slice(0, -1).map(el => parseInt(el));
+const temp = `42
+0`.split('\n').slice(0, -1)
 
-const solution = (arr) => {
-    const max = arr.reduce((el, acc) => Math.max(el, acc));
+if (temp[0] === '') { return; }
+const input = temp.map(el => parseInt(el));
 
-    //에라토스테네스의 채
-    const primeArr = Array(max + 1).fill(true);
+const max = input.reduce((cur, acc) => Math.max(cur, acc));
 
-    //홀수만 구함
-    for (let i = 2; i * i <= max; i++) {
-        if (primeArr[i]) {
-            for (let j = i * i; j <= max; j += i) {
-                primeArr[j] = false;
+const getPrime = (n) => {
+    const primArr = Array(n + 1).fill(true);
+    for (let i = 2; i * i <= n; i++) {
+        if (primArr[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                primArr[j] = false;
             }
         }
     }
+    return primArr;
+}
 
-
-    arr.forEach(n => {
-        //가장 차이가 큰 조합 구하기
-        let flag = false;
-        for (let i = 3; i < parseInt(n/2)+1 ; i += 2) {
-            if (primeArr[i] && primeArr[n - i]) {
-                console.log(`${n} = ${i} + ${n - i}`);
-                flag = true;
-                break;
-            }
+const getMaxDiffCombination = (arr, n) => {
+    for (let i = 3; i <= n / 2; i += 2) {
+        if (arr[i] && arr[n - i]) {
+            return `${n} = ${i} + ${n - i}`;
         }
-        if (!flag) console.log(`Goldbach's conjecture is wrong.`);
+    }
+    return `Goldbach's conjecture is wrong.`;
+}
+
+const solution = (arr) => {
+    if (max === NaN) return;
+    const primArr = getPrime(max);
+    const answer = [];
+    arr.forEach(el => {
+        answer.push(getMaxDiffCombination(primArr, el));
     })
+
+    console.log(answer.join('\n'));
 }
 
 solution(input);
