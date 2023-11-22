385a1ac96b9735ab1b885a336ddc98a609cd0fdc
hisg123 <hisg1201@naver.com>
2023-11-22 19:33:09 +0900

    SOLVE: 2583 영역구하기

--- DFS&BFS/2583영역구하기.js
+++ DFS&BFS/2583영역구하기.js
@@ -5,11 +5,21 @@ const input = `5 7 3
 
 const [M, N, K] = input.shift().split(' ').map(Number);
 
-let graph, visited = '';
-let depth = 1;
+let graph = Array.from(Array(M), () => Array(N).fill(0));
+let visited = Array.from(Array(M), () => Array(N).fill(0));
 
+for (let i = 0; i < K; i++) {
+    const [x1, y1, x2, y2] = input[i].split(' ').map(Number);
+    for (let y = y1; y < y2; y++) {
+        for (let x = x1; x < x2; x++) {
+            graph[y][x] = 1;
+        }
+    }
+}
+
+let count = 0;
 const dfs = (x, y) => {
-    console.log(x, y);
+    count++;
     visited[x][y] = 1;
 
     const move = [[-1, 0], [1, 0], [0, -1], [0, 1]];
@@ -17,42 +27,28 @@ const dfs = (x, y) => {
     for (let i = 0; i < 4; i++) {
         const [dx, dy] = move[i];
 
-        let ax = x + dy;
-        let ay = y + dx;
+        let ax = x + dx;
+        let ay = y + dy;
 
-        if (ax > 0 && ay > 0 && ax < N && ay < M) {
-            if (graph[ay][ax] === 0 && visited[ay][ax] === 0) {
-                dfs(ay, ax);
-                depth ++;
+        if (ax >= 0 && ay >= 0 && ax < M && ay < N) {
+            if (graph[ax][ay] === 0 && visited[ax][ay] === 0) {
+                dfs(ax, ay);
             }
         }
     }
 }
 
-const solution = () => {
-    graph = Array.from(Array(M), () => Array(N).fill(0));
-    visited = Array.from(Array(M), () => Array(N).fill(0));
 
-    for (let i = 0; i < K; i++) {
-        const [x1, y1, x2, y2] = input[i].split(' ').map(Number);
-        for (let y = y1; y < y2; y++) {
-            for (let x = x1; x < x2; x++) {
-                graph[y][x] = 1;
-            }
+let result = [];
+for (let i = 0; i < M; i++) {
+    for (let j = 0; j < N; j++) {
+        count = 0;
+        if (graph[i][j] === 0 && visited[i][j] === 0) {
+            dfs(i, j);
+            result.push(count);
         }
     }
-
-    console.log("🚀 ~ solution ~ graph:", graph)
-
-    for (let i = 0; i < M; i++) {
-        for (let j = 0; j < N; j++) {
-            if (graph[i][j] !== 1 && visited[i][j] === 0) {
-                dfs(i, j);
-            }
-        }
-    }
-
-    console.log(depth);
 }
 
-solution();
+console.log(result.length);
+console.log(result.sort((a, b) => a - b).join(' '));
